54a15fca9045b42a1b812b1985f6ffa9de8190a0
Rags-8 <your-email@example.com>
2026-03-03 23:12:15 +0530

    Update chunkSizeWarningLimit in vite config to resolve deployment errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000 // or any necessary limits like 1600. Usually 1000-2000 is fine.
+  }
 })
